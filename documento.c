565c22faddf9b127b1b1641a3d13baa367c6147f
norber88 <martineztzr@gmail.com>
2015-08-16 20:18:14 +0200

    He realizado una división

---
+++ documento.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+int main ()
+{
+	//esta es la función principal.
+	
+	int a;
+	a = 0;
+
+}
+
+int probando (int x, int y, int z){ //función de división
+	x = 2;
+	y = 4;
+	z = y/2;
+	return z;
+}
+
+
